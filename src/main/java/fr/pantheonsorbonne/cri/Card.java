de77e1a36d241f90de9551c9c04e5478fa599ce3
pascal <dupascal00@gmail.com>
2022-10-11 13:24:58 +0200

    created class card with attr,methods

---
+++ src/main/java/fr/pantheonsorbonne/cri/Card.java
@@ -0,0 +1,16 @@
+package fr.pantheonsorbonne.cri;
+
+public class Card {
+    public static final String[] SYMBOL = {"COEUR","TREFLE","PIQUE","CARREAU"};
+    public static final String[] VALUE = {"AS","ROI","DAME","VALET","10","9","8","7","6","5","4","3","2"};
+    private String[] card;
+    private String symbol;
+    private String value;
+    public Card(String symbol, String value){
+        this.symbol = symbol;
+        this.value = value;
+    }
+
+    public String getValue(){ return symbol;}
+    public String getSymbol(){ return value;}
+}
